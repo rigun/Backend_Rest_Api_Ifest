5bb70ddaf635bd966dd7aea598f30405cb06c683
rio gunawan <rio09tkj@gmail.com>
2018-01-08 11:18:01 +0700

    basicauth

--- ifest/ifest/settings.py
+++ ifest/ifest/settings.py
@@ -80,7 +80,8 @@ REST_FRAMEWORK = {
              
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication', 
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication', 
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
     'DEFAULT_PAGINATIONS_CLASSES':(
